7d5b3a3878876c978b2ca72a7425847337f2c2f0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 08:57:24 +0530

    Register User Function:

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -2,6 +2,11 @@ import UserModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+// Register new user
+export const registerUser = async (req, res) => {
 
+  
+  
+};
 
 
