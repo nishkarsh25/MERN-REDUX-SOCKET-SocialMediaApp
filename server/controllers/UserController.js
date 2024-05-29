a4b590349af9b2ceeb15bc4fa408304153836eb0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:26:31 +0530

    Get All Users Function: getAllUsers Function: Retrieves all users from the MongoDB collection using UserModel.find().
    Response Handling: Removes the password field from each user document before sending the other details in the response. If an error occurs during the database query, it returns a 500 status with the error message.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -20,7 +20,20 @@ export const getUser = async (req, res) => {
   }
 };
 
+// Get all users
+export const getAllUsers = async (req, res) => {
 
+  try {
+    let users = await UserModel.find();
+    users = users.map((user)=>{
+      const {password, ...otherDetails} = user._doc
+      return otherDetails
+    })
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 
 
