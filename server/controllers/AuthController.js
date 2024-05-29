52879a6fcb3921c72a1d93f971441d216a42b50b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 08:59:20 +0530

    Check for Existing User: Queries the database to check if a user with the same username already exists. If an existing user is found, it returns a 400 status with a message indicating that the user already exists.
    Save User to Database: Saves the new user to the database using newUser.save().
    
    Generate JWT Token: Generates a JWT token containing the user's username and id using jsonwebtoken.sign(). The token is signed with a secret key (process.env.JWTKEY) and has an expiration time of 1 hour ({ expiresIn: "1h" }).
    
    Return Response: Returns a 200 status along with the user object and the generated JWT token in the response body.

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -10,7 +10,24 @@ export const registerUser = async (req, res) => {
   req.body.password = hashedPass
   const newUser = new UserModel(req.body);
   const {username} = req.body
-  
+  try {
+    // addition new
+    const oldUser = await UserModel.findOne({ username });
+
+    if (oldUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    // changed
+    const user = await newUser.save();
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+      process.env.JWTKEY,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ user, token });
+  } catch (error) {
+    
+  }
 };
 
 
