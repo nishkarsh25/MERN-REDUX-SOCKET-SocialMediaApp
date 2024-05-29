032266e103bcfd5555529990ff7d22596c178b23
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:15:42 +0530

    Try-Catch Block: Wraps the middleware logic in a try-catch block to handle any potential errors that may occur during execution.

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const secret = process.env.JWTKEY;
 const authMiddleWare = async (req, res, next) => {
-  
+  try {
+    
+  } catch (error) {
+    
+  }
 };
 
 
