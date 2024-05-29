d4e5f090dcd8d01a355cf933b8053ada05d320dd
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 08:59:35 +0530

    Error Handling:

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -26,7 +26,7 @@ export const registerUser = async (req, res) => {
     );
     res.status(200).json({ user, token });
   } catch (error) {
-    
+    res.status(500).json({ message: error.message });
   }
 };
 
