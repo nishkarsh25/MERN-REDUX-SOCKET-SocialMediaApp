57c165863bded293b98fa7ec8fabe5f766df6804
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 10:09:12 +0530

    Route Imports: Route Modules: These import various route modules from their respective files. Each route module handles specific sets of HTTP requests related to authentication, user management, posts, file uploads, chat management, and messages.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 
+// routes
+import AuthRoute from './routes/AuthRoute.js'
+import UserRoute from './routes/UserRoute.js'
+import PostRoute from './routes/PostRoute.js'
+import UploadRoute from './routes/UploadRoute.js'
+import ChatRoute from './routes/ChatRoute.js'
+import MessageRoute from './routes/MessageRoute.js'
 
 
 
