ad43a1b75e1e9eaac92ab25c894f8bd27e22cf1a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:51:48 +0530

    User Chats Function: userChats Function: This function retrieves chats for a specific user. It uses the find() method to search for chats where the members array includes the user ID obtained from the request parameters (req.params.userId). If chats are found, it sends a JSON response with the chat data and a status code of 200. If no chats are found or an error occurs, it sends a JSON response with the error message and a status code of 500.

--- server/controllers/ChatController.js
+++ server/controllers/ChatController.js
@@ -12,5 +12,14 @@ export const createChat = async (req, res) => {
   }
 };
 
-
+export const userChats = async (req, res) => {
+  try {
+    const chat = await ChatModel.find({
+      members: { $in: [req.params.userId] },
+    });
+    res.status(200).json(chat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
