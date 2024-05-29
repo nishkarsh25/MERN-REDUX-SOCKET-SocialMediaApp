2ed8119e8b7ed871162df1a0a56162cdaa69eb64
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 10:23:26 +0530

    Disconnect Event:
    When a client disconnects from the server, the disconnect event is triggered.
    The server removes the disconnected user from the activeUsers array based on their socketId.
    After updating the activeUsers array, the server emits a get-users event to all connected clients, sending them the updated list of active users.

--- socket/server.js
+++ socket/server.js
@@ -18,7 +18,13 @@ const io = require("socket.io")(8800, {
       io.emit("get-users", activeUsers);
     });
   
-    
+    socket.on("disconnect", () => {
+      // remove user from active users
+      activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+      console.log("User Disconnected", activeUsers);
+      // send all active users to all users
+      io.emit("get-users", activeUsers);
+    });
   
     
   });
