8dc04f8d2f4da2540152e06fdef947f476e0b6d8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:26:44 +0530

    Update User Function: updateUser Function: Updates a user's data by their ID in the MongoDB collection using UserModel.findByIdAndUpdate().
    Password Hashing: If a new password is provided, it hashes the password using bcrypt.hash() before updating the user data.
    
    JWT Token Generation: Generates a new JWT token after updating the user data using jsonwebtoken.sign().
    
    Response Handling: Returns the updated user object and the generated JWT token in the response. If an error occurs during the database operation, it returns a 500 status with the error message.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -35,7 +35,41 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+// udpate a user
 
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  // console.log("Data Received", req.body)
+  const { _id, currentUserAdmin, password } = req.body;
+  
+  if (id === _id) {
+    try {
+      // if we also have to update password then password will be bcrypted again
+      if (password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+      }
+      // have to change this
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      const token = jwt.sign(
+        { username: user.username, id: user._id },
+        process.env.JWTKEY,
+        { expiresIn: "1h" }
+      );
+      console.log({user, token})
+      res.status(200).json({user, token});
+    } catch (error) {
+      console.log("Error agya hy")
+      res.status(500).json(error);
+    }
+  } else {
+    res
+      .status(403)
+      .json("Access Denied! You can update only your own Account.");
+  }
+};
 
 
 
