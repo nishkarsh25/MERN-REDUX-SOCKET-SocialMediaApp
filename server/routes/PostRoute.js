7de4baedbe09f8f7b6bc2d9f81a22e2db7c076e3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:47:41 +0530

    Create Router Instance: Create Router Instance: Creates a new instance of the Express router using express.Router(). The router allows you to define routes and associated middleware for handling incoming HTTP requests.

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from '../controllers/PostController.js'
 import authMiddleWare from '../middleware/AuthMiddleware.js'
-
+const router = express.Router()
 
 
 
