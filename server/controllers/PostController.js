4013081991c99a6ef4812f6eb004ad706cb76d92
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:36:27 +0530

    Get Post Function: getPost Function: Retrieves a post by its ID from the database using PostModel.findById(). If the post is found, it sends a success response with the post data. If the post is not found or an error occurs, it sends a 500 status error response.

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -15,7 +15,18 @@ export const createPost = async (req, res) => {
   }
 };
 
+// get a post
 
+export const getPost = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const post = await PostModel.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 
 
