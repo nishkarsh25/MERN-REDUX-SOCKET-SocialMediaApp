a7ccb96b2e810cc4e79bae4a81c3e354e7de58ea
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:29:58 +0530

    Router Definition: Create Router Instance: Creates a new instance of the Express router using express.Router(). The router allows you to define routes and associated middleware for handling incoming HTTP requests.

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { deleteUser, followUser, getAllUsers, getUser, unfollowUser, updateUser } from '../controllers/UserController.js'
 import authMiddleWare from '../middleware/AuthMiddleware.js';
 
-
+const router = express.Router()
 
 
 
