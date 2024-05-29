a55e77dea79911c666ad84a3aadc1bb96c2c027c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:51:24 +0530

    Create Chat Function: createChat Function: This function handles the creation of a new chat. It creates a new instance of the ChatModel with the members field set to an array containing the IDs of the sender and receiver obtained from the request body. Then, it attempts to save the new chat to the database using the save() method. If successful, it sends a JSON response with the saved chat data and a status code of 200. If an error occurs during the process, it sends a JSON response with the error message and a status code of 500.

--- server/controllers/ChatController.js
+++ server/controllers/ChatController.js
@@ -1,6 +1,16 @@
 import ChatModel from "../models/chatModel.js";
 
-
+export const createChat = async (req, res) => {
+  const newChat = new ChatModel({
+    members: [req.body.senderId, req.body.receiverId],
+  });
+  try {
+    const result = await newChat.save();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 
 
