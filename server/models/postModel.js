1805e2489dda13ea0701ec3668e2199269f42700
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:33:42 +0530

    Field Definitions:
    userId: Specifies a field for the ID of the user who created the post. It is of type String and is required.
    desc: Specifies a field for the description or content of the post. It is of type String and is required.
    likes: Specifies a field for an array containing the IDs of users who liked the post.
    createdAt: Specifies a field for the creation timestamp of the post. It is of type Date and defaults to the current date and time when a new post is created.
    image: Specifies a field for the URL of an image attached to the post. It is of type String.

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema(
-  
-  
+  {
+    userId: { type: String, required: true },
+    desc: {type: String, required : true},
+    likes: [],
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    image: String,
+  },
+  {
+    timestamps: true,
+  }
 );
 
 
