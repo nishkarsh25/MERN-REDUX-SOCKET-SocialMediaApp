25a06b91eb15693760369d5a31dd1e10b7c16ccd
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 10:05:44 +0530

    Multer Configuration: Storage Configuration: This code block configures the storage settings for Multer.
    destination: Specifies the directory where uploaded files will be stored. In this case, it's set to "public/images".
    filename: Defines the filename for the uploaded file. In this case, it's set to the name property from the request body.

--- server/routes/UploadRoute.js
+++ server/routes/UploadRoute.js
@@ -2,8 +2,14 @@ import express from 'express'
 const router = express.Router()
 import multer from 'multer'
 
-
-
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
 
 
 
