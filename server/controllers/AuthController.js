afbc1302269eb0449449281bc509eb35903bfae0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 08:58:17 +0530

    Generate Salt:
    Generates a salt using bcrypt.genSalt(10). The number 10 represents the cost factor for generating the salt.
    
    Hash Password: Hashes the user's password using the generated salt with bcrypt.hash().
    
    Create New User: Creates a new instance of UserModel with the hashed password and other data from the request body.

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -5,7 +5,11 @@ import jwt from "jsonwebtoken";
 // Register new user
 export const registerUser = async (req, res) => {
 
-  
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPass
+  const newUser = new UserModel(req.body);
+  const {username} = req.body
   
 };
 
