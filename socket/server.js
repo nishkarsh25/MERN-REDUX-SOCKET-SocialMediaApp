4beae27600a20bd00236d1b1b9e15adeaaafb9ae
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 10:22:56 +0530

    Inside the connection event handler, the server listens for a new-user-add event emitted by the client, indicating that a new user has connected. Upon receiving the new-user-add event, the server checks if the user is already in the activeUsers array. If not, it adds the user to the array along with their userId and socketId. After updating the activeUsers array, the server emits a get-users event to all connected clients, sending them the updated list of active users.

--- socket/server.js
+++ socket/server.js
@@ -7,7 +7,16 @@ const io = require("socket.io")(8800, {
   let activeUsers = [];
   
   io.on("connection", (socket) => {
-    
+    // add new User
+    socket.on("new-user-add", (newUserId) => {
+      // if user is not added previously
+      if (!activeUsers.some((user) => user.userId === newUserId)) {
+        activeUsers.push({ userId: newUserId, socketId: socket.id });
+        console.log("New User Connected", activeUsers);
+      }
+      // send all active users to new user
+      io.emit("get-users", activeUsers);
+    });
   
     
   
