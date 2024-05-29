3e72b2df7ee79b0b3aed7ceb801079e4ac5c6af2
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:58:14 +0530

    Add Message Function: addMessage Function: This function handles the addition of a new message to a chat. It extracts the chatId, senderId, and text from the request body. Then, it creates a new instance of the MessageModel with these extracted values. After that, it attempts to save the new message to the database using the save() method. If successful, it sends a JSON response with the saved message data and a status code of 200. If an error occurs during the process, it sends a JSON response with the error message and a status code of 500.

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -1,5 +1,18 @@
 import MessageModel from "../models/messageModel.js";
 
-
+export const addMessage = async (req, res) => {
+  const { chatId, senderId, text } = req.body;
+  const message = new MessageModel({
+    chatId,
+    senderId,
+    text,
+  });
+  try {
+    const result = await message.save();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 
