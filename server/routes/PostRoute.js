2b5d7eb74f48dc0a6f5ee6eccde0074a562702c2
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:48:01 +0530

    Route Definitions: Create Post Route: Defines a POST route for creating a new post. When a POST request is made to /, the createPost controller function is invoked to handle the request.
    Get Post by ID Route: Defines a GET route for retrieving a post by its ID. When a GET request is made to /:id, the getPost controller function is invoked to handle the request.
    
    Update Post Route: Defines a PUT route for updating a post by its ID. When a PUT request is made to /:id, the updatePost controller function is invoked to handle the request.
    
    Delete Post Route: Defines a DELETE route for deleting a post by its ID. When a DELETE request is made to /:id, the deletePost controller function is invoked to handle the request.
    
    Like/Dislike Post Route: Defines a PUT route for liking or disliking a post by its ID. When a PUT request is made to /:id/like, the likePost controller function is invoked to handle the request.
    
    Get Timeline Posts Route: Defines a GET route for retrieving timeline posts for a user by their ID. When a GET request is made to /:id/timeline, the getTimelinePosts controller function is invoked to handle the request.

--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -3,5 +3,10 @@ import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost
 import authMiddleWare from '../middleware/AuthMiddleware.js'
 const router = express.Router()
 
-
+router.post('/',createPost)
+router.get('/:id', getPost)
+router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
+router.put('/:id/like', likePost)
+router.get('/:id/timeline', getTimelinePosts)
 
