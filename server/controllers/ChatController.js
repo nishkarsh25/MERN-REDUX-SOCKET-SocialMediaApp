b78c1714f70b2aa90e072c141d71b57f2cbd7255
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:52:11 +0530

    Find Chat Function: findChat Function: This function retrieves a chat between two specific users. It uses the findOne() method to search for a chat where the members array contains both user IDs obtained from the request parameters (req.params.firstId and req.params.secondId). If a chat is found, it sends a JSON response with the chat data and a status code of 200. If no chat is found or an error occurs, it sends a JSON response with the error message and a status code of 500.

--- server/controllers/ChatController.js
+++ server/controllers/ChatController.js
@@ -23,3 +23,13 @@ export const userChats = async (req, res) => {
   }
 };
 
+export const findChat = async (req, res) => {
+  try {
+    const chat = await ChatModel.findOne({
+      members: { $all: [req.params.firstId, req.params.secondId] },
+    });
+    res.status(200).json(chat)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+};
