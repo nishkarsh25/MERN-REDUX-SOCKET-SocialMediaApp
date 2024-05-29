0c0de0040231a0de2140fb3d5d4f5ac0f15a4a1b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:59:33 +0530

    Get Messages Function: getMessages Function: This function retrieves messages for a specific chat. It extracts the chatId from the request parameters (req.params.chatId). Then, it uses the find() method to search for messages where the chatId field matches the extracted chatId. If messages are found, it sends a JSON response with the message data and a status code of 200. If no messages are found or an error occurs, it sends a JSON response with the error message and a status code of 500. Summary: This code defines two controller functions for managing message-related operations:
    addMessage: Adds a new message to a chat.
    getMessages: Retrieves messages for a specific chat.
    These functions interact with the MongoDB database using the MessageModel and handle various error scenarios by sending appropriate responses with status codes.

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -15,4 +15,12 @@ export const addMessage = async (req, res) => {
   }
 };
 
-
+export const getMessages = async (req, res) => {
+  const { chatId } = req.params;
+  try {
+    const result = await MessageModel.find({ chatId });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
