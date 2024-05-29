95e9b0b608531f43d9f01b15de2914608bd7c14c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:31:22 +0530

    Follow User Route: Defines a PUT route for following a user by ID. When a PUT request is made to /:id/follow, the authMiddleWare middleware function is first invoked to ensure authentication, and then the followUser controller function is invoked to handle the request.
    Unfollow User Route: Defines a PUT route for unfollowing a user by ID. When a PUT request is made to /:id/unfollow, the authMiddleWare middleware function is first invoked to ensure authentication, and then the unfollowUser controller function is invoked to handle the request.

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -8,6 +8,6 @@ router.get('/:id', getUser);
 router.get('/',getAllUsers)
 router.put('/:id',authMiddleWare, updateUser)
 router.delete('/:id',authMiddleWare, deleteUser)
-
-
+router.put('/:id/follow',authMiddleWare, followUser)
+router.put('/:id/unfollow',authMiddleWare, unfollowUser)
 
