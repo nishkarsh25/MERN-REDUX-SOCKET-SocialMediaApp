9e218074c84f2eff98f35c77534d28d52956b40a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:27:30 +0530

    Follow and Unfollow User Functions: followUser and unfollowUser Functions: Add or remove a user's ID from the followers and following arrays of another user's document in the MongoDB collection.
    Authorization Check: Checks if the current user is trying to follow/unfollow themselves and returns an error if they are.
    
    Response Handling: Returns a success message if the operation is successful. If an error occurs during the database operation, it returns a 500 status with the error message.
    
    Summary:
    This code defines several controller functions for handling user-related operations such as retrieving user data, updating user data, deleting users, following users, and unfollowing users. These functions interact with the MongoDB database using the UserModel and handle authentication and authorization using JWT tokens and user IDs.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -116,4 +116,33 @@ export const followUser = async (req, res) => {
   }
 };
 
+// Unfollow a User
+// changed
+export const unfollowUser = async (req, res) => {
+  const id = req.params.id;
+  const { _id } = req.body;
 
+  if(_id === id)
+  {
+    res.status(403).json("Action Forbidden")
+  }
+  else{
+    try {
+      const unFollowUser = await UserModel.findById(id)
+      const unFollowingUser = await UserModel.findById(_id)
+
+
+      if (unFollowUser.followers.includes(_id))
+      {
+        await unFollowUser.updateOne({$pull : {followers: _id}})
+        await unFollowingUser.updateOne({$pull : {following: id}})
+        res.status(200).json("Unfollowed Successfully!")
+      }
+      else{
+        res.status(403).json("You are not following this User")
+      }
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  }
+};
