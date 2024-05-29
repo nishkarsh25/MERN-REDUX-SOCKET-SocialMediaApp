a066622c9a847aa77bea8783fa5e36be8b4e9fe9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:47:23 +0530

    Import Statements: Express Import: Imports the express library, which is a web application framework for Node.js. It provides a robust set of features for building web servers and APIs.
    Controller Functions Import: Imports the controller functions (createPost, getPost, updatePost, deletePost, likePost, getTimelinePosts) from their respective controller file (PostController.js). These functions handle various post-related operations such as creating posts, retrieving posts, updating posts, deleting posts, liking/disliking posts, and retrieving timeline posts.
    
    Authentication Middleware Import: Imports the authMiddleWare middleware function from the AuthMiddleware.js file. This middleware function ensures that routes are protected and require authentication before accessing them.

---
+++ server/routes/PostRoute.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from '../controllers/PostController.js'
+import authMiddleWare from '../middleware/AuthMiddleware.js'
+
+
+
+
