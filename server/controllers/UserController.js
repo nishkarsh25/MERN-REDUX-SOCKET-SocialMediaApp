17cf7e1df2453fb8fac8a462245292723cd9818c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:25:44 +0530

    Get User Function: getUser Function: Retrieves a user's data by their ID from the MongoDB collection using UserModel.findById().
    Response Handling: If the user is found, it removes the password field from the user document before sending the other details in the response. If the user is not found, it returns a 404 status with a message indicating that the user was not found. If an error occurs during the database query, it returns a 500 status with the error message.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,7 +2,23 @@ import UserModel from "../models/userModel.js";
 
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken'
+// Get a User
+export const getUser = async (req, res) => {
+  const id = req.params.id;
 
+  try {
+    const user = await UserModel.findById(id);
+    if (user) {
+      const { password, ...otherDetails } = user._doc;
+
+      res.status(200).json(otherDetails);
+    } else {
+      res.status(404).json("No such User");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 
 
