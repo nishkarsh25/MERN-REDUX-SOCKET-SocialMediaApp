720c25ddb2aab13cfe006a7cc42e3a8aa2872c80
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:36:15 +0530

    Create Post Function: createPost Function: Handles the creation of a new post. It creates a new instance of the PostModel using the request body data, saves it to the database using save(), and sends a success response with the created post data. If an error occurs during the process, it sends a 500 status error response.

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -2,7 +2,18 @@ import PostModel from "../models/postModel.js";
 import UserModel from "../models/userModel.js";
 import mongoose from "mongoose";
 
+// creating a post
 
+export const createPost = async (req, res) => {
+  const newPost = new PostModel(req.body);
+
+  try {
+    await newPost.save();
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 
 
