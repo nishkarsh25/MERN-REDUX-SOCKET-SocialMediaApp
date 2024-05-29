3427e6706f6e2bdffb17aebc97485cf653ca7758
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-29 09:00:52 +0530

    Find User in Database: Queries the database to find a user with the provided username using UserModel.findOne().
    Check Password Validity: Compares the provided password with the hashed password stored in the database using bcrypt.compare(). If the passwords match, it proceeds; otherwise, it returns a 400 status with a message indicating that the password is incorrect.
    
    Generate JWT Token: Generates a JWT token similar to the register user function.
    
    Return Response: Returns a 200 status along with the user object and the generated JWT token in the response body if the login is successful. If the user is not found, it returns a 404 status with a message indicating that the user was not found.

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -36,5 +36,26 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
 
-  
+  try {
+    const user = await UserModel.findOne({ username: username });
+
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+
+      if (!validity) {
+        res.status(400).json("wrong password");
+      } else {
+        const token = jwt.sign(
+          { username: user.username, id: user._id },
+          process.env.JWTKEY,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ user, token });
+      }
+    } else {
+      res.status(404).json("User not found");
+    }
+  } catch (err) {
+    
+  }
 };
