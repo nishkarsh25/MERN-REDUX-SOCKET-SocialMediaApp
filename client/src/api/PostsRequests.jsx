ef4d85b2b317f4ccfdaf69711964bb693b3fca28
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:00:13 +0530

    Details of the Interceptor const token = JSON.parse(localStorage.getItem('profile'))?.token; This line attempts to retrieve a token from the local storage. It parses the JSON string stored under the key 'profile' and extracts the 'token' property. if (token) { req.headers.Authorization = token; } If a token is found, it is added to the request headers as Authorization. The token is passed without a "Bearer" prefix.

--- client/src/api/PostsRequests.jsx
+++ client/src/api/PostsRequests.jsx
@@ -3,7 +3,12 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
-    
+    const token = JSON.parse(localStorage.getItem('profile'))?.token;
+    if (token) {
+        req.headers.Authorization = token; // Pass the token without the "Bearer" prefix
+    }
+
+    return req;
 });
 
 
