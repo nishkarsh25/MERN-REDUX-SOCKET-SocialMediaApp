2de76afb08ab6d87487ca2709be6552aa5e544fe
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:06:59 +0530

    Async Thunks Asynchronous actions are created using createAsyncThunk. They handle side effects like API calls.
    logIn
    Purpose: Handles user login.
    Arguments: Accepts formData (user credentials) and navigate (for programmatic navigation).
    Logic:
    Tries to send a login request using AuthApi.logIn(formData).
    On success, it navigates to /home and returns the received data.
    On failure, it returns an error payload using rejectWithValue.

--- client/src/slice/authSlice.jsx
+++ client/src/slice/authSlice.jsx
@@ -1,7 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import * as AuthApi from '../api/AuthRequests';
 
-
+// Async thunks
+export const logIn = createAsyncThunk(
+  'auth/logIn',
+  async ({ formData, navigate }, { rejectWithValue }) => {
+    try {
+      console.log(formData)
+      const { data } = await AuthApi.logIn(formData);
+      console.log(data)
+      navigate('/home', { replace: true });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
 
 
