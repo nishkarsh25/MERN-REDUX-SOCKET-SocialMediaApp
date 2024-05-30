5b86c1383bbbb1d3fc4b763dd0c7b38ec8b06846
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:00:39 +0530

    likePost Function
    export const likePost = (id, userId) => API.put(posts/${id}/like, { userId: userId });
    This function sends a PUT request to the endpoint /posts/{id}/like to like a specific post.
    id is the identifier of the post to be liked.
    userId is the identifier of the user who is liking the post.
    The request payload contains an object with the userId. The API.put method is used to send the request, and the full URL for this request will be http://localhost:5000/posts/{id}/like.

--- client/src/api/PostsRequests.jsx
+++ client/src/api/PostsRequests.jsx
@@ -12,7 +12,7 @@ API.interceptors.request.use((req) => {
 });
 
 export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`);
-
+export const likePost = (id, userId) => API.put(`posts/${id}/like`, { userId: userId });
 
 
 
