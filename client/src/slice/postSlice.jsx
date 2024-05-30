8179f263d784efe6791f354c202a1bf8ef960e9f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:15:23 +0530

    Imports:
    The code imports createAsyncThunk and createSlice from @reduxjs/toolkit.
    It also imports API functions from the PostsApi module.
    Async Thunks:
    
    Three async thunks are defined:
    getTimelinePosts: Retrieves timeline posts.
    uploadPost: Uploads a new post.
    likePost: Likes a post.
    Each async thunk is created using createAsyncThunk, which takes a name for the action and an async function.
    Inside each async function, API calls are made using functions from PostsApi. These functions are awaited, and their responses are processed.
    In case of an error, rejectWithValue is used to return the error payload.

--- client/src/slice/postSlice.jsx
+++ client/src/slice/postSlice.jsx
@@ -1,7 +1,45 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as PostsApi from '../api/PostsRequests';
 
+// Async thunk for getting timeline posts
+export const getTimelinePosts = createAsyncThunk(
+  'posts/getTimelinePosts',
+  async (id, thunkAPI) => {
+    try {
+      const { data } = await PostsApi.getTimelinePosts(id);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
+// Async thunk for uploading a post
+export const uploadPost = createAsyncThunk(
+  'upload/uploadPost',
+  async (data, thunkAPI) => {
+    try {
+      const newPost = await PostsApi.uploadPost(data);
+      return newPost.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Async thunk for liking a post
+export const likePost = createAsyncThunk(
+  'posts/likePost',
+  async (payload, thunkAPI) => {
+    try {
+      const { postId, userId } = payload;
+      await PostsApi.likePost(postId, userId);
+      return postId; // Return the postId if the like is successful
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 
 
