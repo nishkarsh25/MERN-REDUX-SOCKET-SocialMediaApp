5b1e3534c4b32cd782d775cd67d0e4ef762499b8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:22:02 +0530

    Local Storage Functions:
    Two functions are defined:
    saveToLocalStorage(store): This function takes the Redux store as input, serializes it to JSON, and saves it to the browser's local storage under the key 'store'.
    loadFromLocalStorage(): This function retrieves the serialized store from local storage, parses it back into JavaScript objects, and returns it. If there's no data in local storage or an error occurs during parsing, it returns undefined.
    These functions are used for persisting and loading the Redux store state to/from the browser's local storage.

--- client/src/store/ReduxStore.js
+++ client/src/store/ReduxStore.js
@@ -7,9 +7,25 @@ import userReducer from '../slice/userSlice';
 import messageReducer from '../slice/messageSlice';
 
 
+function saveToLocalStorage(store) {
+    try {
+        const serializedStore = JSON.stringify(store);
+        window.localStorage.setItem('store', serializedStore);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-
-
+function loadFromLocalStorage() {
+    try {
+        const serializedStore = window.localStorage.getItem('store');
+        if (serializedStore === null) return undefined;
+        return JSON.parse(serializedStore);
+    } catch (e) {
+        console.log(e);
+        return undefined;
+    }
+}
 
 
 
