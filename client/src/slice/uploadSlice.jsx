2f0dd7151487fef3e4dcf81878a529229be94f70
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:16:44 +0530

    Async Thunk:
    An async thunk named uploadImage is defined.
    It is created using createAsyncThunk, taking a name for the action and an async function.
    Inside the async function, an API call is made using UploadApi.uploadImage(data).
    If the API call is successful, nothing specific happens, but if it fails, the rejectWithValue function is used to return the error payload.

--- client/src/slice/uploadSlice.jsx
+++ client/src/slice/uploadSlice.jsx
@@ -1,7 +1,18 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as UploadApi from '../api/UploadRequest';
 
-
+// Async thunk for uploading an image
+export const uploadImage = createAsyncThunk(
+  'upload/uploadImage',
+  async (data, thunkAPI) => {
+    try {
+      console.log('Image upload Action start ho gya hy');
+      await UploadApi.uploadImage(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 
 
