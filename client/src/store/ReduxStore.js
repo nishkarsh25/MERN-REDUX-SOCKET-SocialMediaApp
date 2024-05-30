07c2d1b53b3f074b3bd42776b183a407aee6e75e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:21:48 +0530

    Imports:
    The code imports configureStore and combineReducers from @reduxjs/toolkit.
    It also imports the reducers (authReducer, chatReducer, postReducer, uploadReducer, userReducer, messageReducer) from their respective slices or files.

---
+++ client/src/store/ReduxStore.js
@@ -0,0 +1,22 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import authReducer from '../slice/authSlice';
+import chatReducer from '../slice/chatSlice';
+import postReducer from '../slice/postSlice';
+import uploadReducer from '../slice/uploadSlice';
+import userReducer from '../slice/userSlice';
+import messageReducer from '../slice/messageSlice';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
