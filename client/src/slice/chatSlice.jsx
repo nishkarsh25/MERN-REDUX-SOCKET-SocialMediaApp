9f874f821725813e74b90faffb68f2e21dc9913c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:12:04 +0530

    Async Thunks:
    Async thunks are functions that can contain asynchronous logic and dispatch actions based on the result.
    Three async thunks are defined:
    createChat: For creating a new chat.
    userChats: For retrieving chats associated with a user.
    findChat: For finding a chat between two users.
    Each async thunk is created using createAsyncThunk, which takes a name for the action and an async function.
    Inside each async function, API calls are made using functions from ChatApi. These functions are awaited, and their responses are returned.
    In case of an error, rejectWithValue is used to return the error payload.

--- client/src/slice/chatSlice.jsx
+++ client/src/slice/chatSlice.jsx
@@ -1,11 +1,44 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as ChatApi from '../api/ChatRequests';
 
+// Async thunk for creating a chat
+export const createChat = createAsyncThunk(
+  'chat/createChat',
+  async (data, thunkAPI) => {
+    try {
+      const response = await ChatApi.createChat(data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
+// Async thunk for getting user chats
+export const userChats = createAsyncThunk(
+  'chat/userChats',
+  async (id, thunkAPI) => {
+    try {
+      const response = await ChatApi.userChats(id);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
-
-
-
+// Async thunk for finding a chat between two users
+export const findChat = createAsyncThunk(
+  'chat/findChat',
+  async ({ firstId, secondId }, thunkAPI) => {
+    try {
+      const response = await ChatApi.findChat(firstId, secondId);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 
 
