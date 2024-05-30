72dbec8b3124944a863860b7aecc29ef65c2f890
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:18:56 +0530

    Async Thunks:
    Four async thunks are defined:
    updateUser: Used for updating user data.
    followUser: Used for following a user.
    unfollowUser: Used for unfollowing a user.
    fetchUser: Used for fetching user data.
    Each async thunk is created using createAsyncThunk.
    Each thunk takes parameters necessary for the API call and a thunkAPI object.
    Inside each thunk's async function, an API call is made using corresponding functions from the UserApi module.
    If the API call is successful, the response data is returned. If it fails, rejectWithValue is used to return the error payload.

--- client/src/slice/userSlice.jsx
+++ client/src/slice/userSlice.jsx
@@ -1,7 +1,45 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import * as UserApi from '../api/UserRequests';
 
+// Async thunk for updating user data
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async ({ id, formData }, { rejectWithValue }) => {
+    try {
+      const response = await UserApi.updateUser(id, formData);
+      console.log(formData)
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
+// Async thunk for following a user
+export const followUser = createAsyncThunk(
+  'user/followUser',
+  async ({ id, data }, { rejectWithValue }) => {
+    try {
+      const response = await UserApi.followUser(id, data);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Async thunk for unfollowing a user
+export const unfollowUser = createAsyncThunk(
+  'user/unfollowUser',
+  async ({ id, data }, { rejectWithValue }) => {
+    try {
+      const response = await UserApi.unfollowUser(id, data);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
 
 
