f40e914e1141df1b3321a1e2b8661266af0ef777
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 07:13:49 +0530

    Async Thunks:
    Async thunks are functions that can contain asynchronous logic and dispatch actions based on the result.
    Two async thunks are defined:
    getMessages: For retrieving messages.
    addMessage: For adding a new message.
    Each async thunk is created using createAsyncThunk, which takes a name for the action and an async function.
    Inside each async function, API calls are made using functions from MessageApi. These functions are awaited, and their responses are returned.
    In case of an error, rejectWithValue is used to return the error payload.

--- client/src/slice/messageSlice.jsx
+++ client/src/slice/messageSlice.jsx
@@ -1,9 +1,32 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as MessageApi from '../api/MessageRequests';
 
+// Async thunk for getting messages
+export const getMessages = createAsyncThunk(
+  'messages/getMessages',
+  async (id, thunkAPI) => {
+    try {
+      const response = await MessageApi.getMessages(id);
+      console.log(response.data)
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
-
-
+// Async thunk for adding a message
+export const addMessage = createAsyncThunk(
+  'messages/addMessage',
+  async (data, thunkAPI) => {
+    try {
+      const response = await MessageApi.addMessage(data);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 
 
