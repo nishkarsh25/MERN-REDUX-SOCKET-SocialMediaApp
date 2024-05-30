7a6bfb1c84634274857e11b4fc50bd8a312d7bd0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 06:56:42 +0530

    userChats Function
    export const userChats = (id) => API.get(/chat/${id});
    This function sends a GET request to the endpoint /chat/{id} to retrieve all chat data for a specific user.
    id is the user ID whose chats you want to retrieve.
    The API.get method is used to send the request, and the full URL for this request will be http://localhost:5000/chat/{id}.

--- client/src/api/ChatRequests.jsx
+++ client/src/api/ChatRequests.jsx
@@ -5,5 +5,5 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 export const createChat = (data) => API.post('/chat/', data);
 
-
+export const userChats = (id) => API.get(`/chat/${id}`);
 
