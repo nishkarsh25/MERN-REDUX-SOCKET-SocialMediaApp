b02cd1d22a3bddad08897110eeea594217ed6e2b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-30 06:58:12 +0530

    addMessage Function
    export const addMessage = (data) => API.post('/message/', data);
    This function sends a POST request to the endpoint /message/ to add a new message to a chat or conversation.
    data is an object containing the information needed to create a new message (such as the message content, sender ID, chat ID, timestamp, etc.).
    The API.post method is used to send the request, and the full URL for this request will be http://localhost:5000/message/.

--- client/src/api/MessageRequests.jsx
+++ client/src/api/MessageRequests.jsx
@@ -5,3 +5,4 @@ const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 
+export const addMessage = (data) => API.post('/message/', data);
